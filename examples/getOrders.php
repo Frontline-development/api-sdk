297a54c996d90d8ce0b4c3d3b3c7ac4a430648be
mattj <mattj@carmmunuty.co.uk>
2021-01-29 11:53:07 +0000

    example- class path

--- examples/getOrders.php
+++ examples/getOrders.php
@@ -4,6 +4,6 @@ require __DIR__ . '../vendor/autoload.php';
 $token = "example_token";
 $secret = "example_secret";
 
-$fl = new Frontline\Frontline($token, $secret);
+$fl = new Frontline($token, $secret);
 
 var_dump($fl->get('/api/orders'));
